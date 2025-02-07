3b1ca04390b5f30f56323201d767dc82f7bf3cf7
Reloaded <deepak@designandcode.tech>
2025-02-07 21:10:19 +0530

    contact page template

--- template-parts/page-contact-us.php
+++ template-parts/page-contact-us.php
@@ -8,44 +8,46 @@ get_header();
 <div class="c-page-blk--out"></div>
 <div class="c-transition-blk-out--menu"></div>
 <main class="wrapper">
-	<div class="contact-hero" style="background-image: url(https://heigh10.com/wp-content/uploads/2023/03/contact-us-banner.jpg);">
-		<div class="bottom-shadow">
-			<p>/ Contact us</p>
-		<h1>
-			Let's get the<br>
-		ball rolling.
-		</h1>
-		</div>
-		</div>
-	<div data-w-id="1b175ab3-d1ad-baea-6bb5-59e25653e69b" class="c-contact-hero wf-section" style="background: #000;">
+	<div class="contact-hero"
+		style="background-image: url(https://heigh10.com/wp-content/uploads/2023/03/contact-us-banner.jpg);">
 		<div class="d-flex">
-			<div class="c-grid-block__p4 cc--contact">
+			<div class="c-grid-block cc--contact">
+				<?php the_title( '<p>/ ', '</p>'); ?>
+				</p>
 				<div class="c-hero--statement Contact" data-w-id="6b0044af-cfeb-c4a5-c4cd-13a67fc3d0e3"
-				style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);opacity:0;">
-					<h1 class="c-hero--txt" style="color: #fff;">Contact</h1>
-					<h1 class="c-hero--txt is--large long-gradient">Get in
-						touch</h1>
+					style="-webkit-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);-moz-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);-ms-transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);transform:translate3d(0, 40PX, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 2DEG);opacity:0;">
+					<h1 class="c-hero--txt is--large long-gradient">Get in touch</h1>
 				</div>
 			</div>
 			<div class="cc-form">
-				<?php the_content(); ?>
+				<?php 
+		echo do_shortcode('[elementor-template id="910"]');
+			 ?>
 			</div>
-			
 		</div>
-		
+		<div class="bottom-shadow"></div>
+	</div>
+
+
+	<?php
+	the_content();
+	?>
+
+
+	<div data-w-id="1b175ab3-d1ad-baea-6bb5-59e25653e69b" class="c-contact-hero wf-section" style="background: #000;">
 		<a data-w-id="65d4a801-7e19-2555-6edb-08eab5b4201b" href="mailto:hello@heigh10.com?subject=Hi%20Heigh10!"
 			class="c-full-width-title-block cc--contact w-inline-block" style="background: #fff;">
 			<div class="c-link__title-wrapper">
-        <div data-split="100" data-split-type="chars" class=
-        "c-title-block__text" style="text-align:center;color: #000;">
-          hello@heigh10.com
-        </div>
-        <div data-w-id="6b0044af-cfeb-c4a5-c4cd-13a67fc3d0e3" style=
-        "-webkit-transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity: 1;"
-        class="c-title-block__text cc--absolute is--centred">
-          send it
-        </div>
-      </div>
+				<div data-split="100" data-split-type="chars" class="c-title-block__text"
+					style="text-align:center;color: #000;">
+					hello@heigh10.com
+				</div>
+				<div data-w-id="6b0044af-cfeb-c4a5-c4cd-13a67fc3d0e3"
+					style="-webkit-transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(-50%, null, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity: 1;"
+					class="c-title-block__text cc--absolute is--centred">
+					send it
+				</div>
+			</div>
 		</a>
 	</div>
 </main>
